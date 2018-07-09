41f40498a2f29a501620b3f703792e514ee9200a
Filip Stepien <contact@filipstepien.com>
2018-07-09 08:47:23 -0400

    add dev tools and hot reload

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react';
 
 export default class Comments extends Component {
+  constructor() {
+    super();
+    this.renderComment = this.renderComment.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
   renderComment(comment, i) {
     return (
       <div className="comment" key={i}>
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className="remove-comments">&times;</button>
+          <button
+            className="remove-comments"
+            onClick={() =>
+              this.props.removeComment(this.props.params.postId, i)
+            }
+          >
+            &times;
+          </button>
         </p>
       </div>
     );
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    const { postId } = this.props.params;
+    const author = this.refs.author.value;
+    const comment = this.refs.comment.value;
+    this.props.addComment(postId, author, comment);
+    this.refs.commentForm.reset();
   }
   render() {
     return (
       <div className="comments">
         {this.props.postComments.map(this.renderComment)}
-        <form ref="commentForm" className="comment-form">
+        <form
+          ref="commentForm"
+          className="comment-form"
+          onSubmit={this.handleSubmit}
+        >
           <input type="text" ref="author" placeholder="author" />
           <input type="text" ref="comment" placeholder="comment" />
           <input type="submit" hidden />
